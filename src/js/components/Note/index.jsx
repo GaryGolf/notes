dcb6805b796699771bf5dd08ac6fa8d43695cdb0
GaryGolf <garygolf@mail.ru>
2016-08-15 20:06:23 +0300

    textarea

--- src/js/components/Note/index.jsx
+++ src/js/components/Note/index.jsx
@@ -1,31 +1,55 @@
 import React from 'react';
 import './note.css';
-import trash from './trash.png';
+import trash from './times.png';
+import tick from './tick.png';
 
 export default class  Note extends React.Component {
     constructor(props) {
         super(props);
         this.displayName = 'Hello';
+        this.state = {
+                editMode: false,
+                value: this.props.children
+            };
     }
     clickHandler(e){
-        console.log(e.type);
+        this.setState({editMode:false});
     }
     dblClickHandler(e){
-        console.log(e.type)
+        this.setState({editMode:true});
     }
+    changeTextHandler(e) {
+        this.setState({
+                value: e.target.value
+            });
+    }
+    renderTextInput() {
+        return (
+
+            <div className="note">
+                <textarea className="note" onInput={this.changeTextHandler.bind(this)}
+                    value = {this.state.value} />
+                <span className="tick">
+                    <img src={tick} onClick={this.clickHandler.bind(this)} />
+                </span>
+
+            </div>
+
+            );
+    }
+
     render() {
-        return ( 
+        if(this.state.editMode) return this.renderTextInput();
+        else return ( 
         	<div 
                 className='note' 
                 onDoubleClick={this.dblClickHandler.bind(this)}
                 onContextMenu={this.dblClickHandler.bind(this)}
             >
-                   {this.props.children}
-                <span className="btns">
+                   {this.state.value}
+                <span className="trash">
                     <span>
-                       
-                            <img src={trash} onClick={this.clickHandler.bind(this)} />
-           
+                         <img src={trash} onClick={this.clickHandler.bind(this)} />
                     </span>
                 </span>
 
